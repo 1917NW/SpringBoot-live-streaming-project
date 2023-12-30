337000998836ba6ccd37208c4950ace92d8e7231
li <88031594+1917NW@users.noreply.github.com>
2023-12-30 20:47:44 +0800

    init project

---
+++ src/main/java/org/lxy/SpringbootLiveStreamApplication.java
@@ -0,0 +1,13 @@
+package org.lxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLiveStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLiveStreamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/lxy/SpringbootLiveStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLiveStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
